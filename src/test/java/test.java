4394d6c07156dd85c6d248d446f2293858c54b92
zoricaarsenovic <zorica.arsenovic1@gmail.com>
2025-05-13 14:17:49 -0700

    firstcommit

---
+++ src/test/java/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+        System.out.println("This is the project version 1");
+
+
+
+    }
+}
+// done with version one
